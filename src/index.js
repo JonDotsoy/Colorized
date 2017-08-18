40a6aa40e26511088f8ac1623d240d69ec186bd6
Jonathan Delgado Zamorano <hi@jon.soy>
2017-08-17 23:12:16 -0300

    Added button actions

--- src/index.js
+++ src/index.js
@@ -1,107 +1,8 @@
-const {injectGlobal, default: styled, ThemeProvider} = require('styled-components')
 const _ = global._ =  require('lodash')
 const Color = global.Color = require('color')
 const React = require('react')
 const ReactDOM = require('react-dom')
-const MdContentCopy = require('react-icons/lib/md/content-copy')
-const MdAutorenew = require('react-icons/lib/md/autorenew')
-const MdCreate = require('react-icons/lib/md/create')
-
-injectGlobal`
-  @import url('https://fonts.googleapis.com/css?family=Roboto+Mono:100,300,400,500,700');
-  @import url('https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700');
-  html {
-    font-family: 'Roboto', sans-serif;
-    font-weight: 200;
-  }
-
-  body {
-    padding: 0px;
-    margin: 0px;
-  }
-`
-
-const Header = styled.div`
-    
-`
-
-const BodyContainer = styled.div`
- 
-  padding: 0px;
-  min-height: 100vh;
-  padding: 10px;
-  box-sizing: border-box;
-  display: flex;
-  width: 100%;
-  flex-direction: column;
-`
-
-const LoadColorText = props => (
-  Color(props.theme.color).dark()
-    ? props.theme.colorTextLight
-    : props.theme.colorTextDark
-)
-
-const Brand = styled.label`
-  font-family: 'Roboto', sans-serif;
-  text-transform: uppercase;
-  font-size: 1.2em;
-  font-weight: 400;
-  margin: 0px;
-  color: ${LoadColorText};
-`
-
-const TextColor = styled.label`
-  font-family: 'Roboto Mono', monospace;
-  color: ${LoadColorText};
-  font-weight: 100;
-  font-size: 3em;
-  margin: 0px;
-  text-transform: uppercase;
-  text-align: center;
-  display: block;
-  align-self: center;
-`
-
-const ContainerTextColor = styled.div`
-  flex: 1;
-  display: flex;
-  justify-content: center;
-`
-
-const InputSelectColors = styled.div`
-  display: none;
-`
-
-const Footer = styled.div`
-  text-transform: none;
-  text-decoration: none;
-  color: ${LoadColorText};
-  a:visited,
-  a:link,
-  a {
-    color: ${LoadColorText};
-    text-decoration: none;
-    font-weight: 300;
-  }
-  a:after {
-    content: ' — ';
-  }
-  a:last-child:after {
-    content: '';
-  }
-`
-
-const BtnClipColor = styled.button`
-  display: inline-block;
-  border: none;
-  background: none;
-  font-size:2em;
-  margin: 0px;
-  padding: 0px;
-  color: ${LoadColorText};
-  opacity: 0.7;
-`
+const {Render} = require('./visor')
 
 function updateHASHLink (txt) {
   // document?.location?.hash = txt
@@ -119,12 +20,48 @@ class App extends React.Component {
     super(props)
     this.state = {
       theme: {
-        color: Color(document.location.hash).hex() || Color.rgb(_.random(0, 255), _.random(0, 255), _.random(0, 255)).hex(),
+        color: updateHASHLink(document.location.hash === '' ? Color.rgb(_.random(0, 255), _.random(0, 255), _.random(0, 255)).hex() : Color(document.location.hash).hex()),
         colorTextLight: '#FFF',
         colorTextDark: '#000'
-      }
+      },
+      paletteView: false,
+      colors: [],
     }
+
     this.inputColor = null
+  }
+
+  componentWillMount () {
+    window.onhashchange = () => {
+       // do something awesome here
+       this.updateColor(Color(document.location.hash).hex())
+    }
+    document.addEventListener('keydown', (event) => {
+      if (event.keyCode === 32) {
+        this.randomColor()
+      }
+    })
+  }
+
+  getColors = () => {
+    return [
+      ...this.state.colors,
+      this.state.theme.color
+    ]
+  }
+
+  handleAddColor = () => {
+    this.setState(({colors, theme: { color }}) => {
+
+      return {
+        paletteView: true,
+        colors: [
+          ...colors,
+          color
+        ]
+      }
+
+    })
   }
 
   updateColor = (color) => {
@@ -138,20 +75,8 @@ class App extends React.Component {
     }))
   }
 
-  RandomColor = () => {
+  randomColor = () => {
     this.updateColor(Color.rgb(_.random(0, 255), _.random(0, 255), _.random(0, 255)).hex())
-  }
-
-  componentWillMount () {
-    window.onhashchange = () => {
-       // do something awesome here
-       this.updateColor(Color(document.location.hash).hex())
-    }
-    document.addEventListener('keydown', (event) => {
-      if (event.keyCode === 32) {
-        this.RandomColor()
-      }
-    })
   }
 
   handleChangeColor = (event) => {
@@ -165,33 +90,22 @@ class App extends React.Component {
     this.refcolor.click()
   }
 
+  handleToggleViewPalette = () => {
+    this.setState(({paletteView}) => ({
+      paletteView: !paletteView
+    }))
+  }
+
   render () {
     return (
-      <div>
-        <ThemeProvider theme={this.state.theme}>
-          <BodyContainer style={{
-             backgroundColor: this.state.theme.color
-          }}>
-
-            <Header>
-              <Brand>Colorized</Brand>
-              <InputSelectColors>
-                <input ref={r=>this.refcolor=r} id="select-color" type='color' value={this.state.theme.color} onChange={this.handleChangeColor} />
-              </InputSelectColors>
-            </Header>
-
-            <ContainerTextColor>
-              <TextColor>{this.state.theme.color}</TextColor>
-            </ContainerTextColor>
-
-            <Footer>
-              <a href="https://github.com/JonDotsoy/Colorized" target="_blank">Github</a>
-              <a href="https://jon.soy/" target="_blank">JON.SOY</a>
-            </Footer>
-
-          </BodyContainer>
-        </ThemeProvider>
-      </div>
+      <Render
+        paletteView={this.state.paletteView}
+        theme={this.state.theme}
+        colors={this.getColors()}
+        randomColor={this.randomColor}
+        handleAddColor={this.handleAddColor}
+        handleToggleViewPalette={this.handleToggleViewPalette}
+      />
     )
   }
 }
---
+++ src/visor.js
@@ -0,0 +1,209 @@
+const {keyframes, injectGlobal, default: styled, ThemeProvider, css} = require('styled-components')
+const MdClear = require('react-icons/lib/md/clear')
+const Color = global.Color = require('color')
+const React = require('react')
+
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=Roboto+Mono:100,300,400,500,700');
+  @import url('https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700');
+
+  html {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 300;
+  }
+
+  body {
+    padding: 0px;
+    margin: 0px;
+  }
+`
+
+const LoadColorText = props => (
+  Color(props.theme.color).dark()
+    ? props.theme.colorTextLight
+    : props.theme.colorTextDark
+)
+
+const TextColorStyle = css`
+  color: ${LoadColorText};
+`
+
+const Header = styled.div`
+    
+`
+
+const BodyContainer = styled.div`
+  padding: 0px;
+  min-height: 100vh;
+  padding: 10px;
+  box-sizing: border-box;
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+`
+
+const Brand = styled.label`
+  font-family: 'Roboto', sans-serif;
+  text-transform: uppercase;
+  font-size: 1.2em;
+  font-weight: 400;
+  margin: 0px;
+  ${TextColorStyle};
+`
+
+const TextColor = styled.label`
+  font-family: 'Roboto Mono', monospace;
+  ${TextColorStyle};
+  font-weight: 100;
+  font-size: 3em;
+  margin: 0px;
+  text-transform: uppercase;
+  text-align: center;
+  display: block;
+  align-self: center;
+`
+
+const ContainerTextColor = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  position: relative;
+`
+
+const Footer = styled.div`
+  text-transform: none;
+  text-decoration: none;
+  ${TextColorStyle};
+  a:visited,
+  a:link,
+  a {
+    ${TextColorStyle};
+    text-decoration: none;
+    font-weight: 300;
+  }
+  a:after {
+    content: ' — ';
+  }
+  a:last-child:after {
+    content: '';
+  }
+`
+
+const keyframesHidden = keyframes`
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+`
+
+const MessagePressEspaceToRandomColor = styled.span`
+  position: absolute;
+  font-size: 1em;
+  text-transform: none;
+  text-decoration: none;
+  ${TextColorStyle};
+  bottom: 0px;
+  font-weight: 300;
+  opacity: 0;
+  animation-delay: 4s;
+  animation: ${keyframesHidden} 2s linear 1;
+`
+
+const LeftActionsHeader = styled.div`
+  float: right;
+`
+
+const ContainerListColors = styled.div`
+  display: flex;
+  position: relative;
+  flex: 1;
+`
+
+const ColorItem = styled.div`
+  background-color: red;
+  flex: 1;
+  ${TextColorStyle};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: relative;
+  text-align: center;
+`
+
+const ColorItemText = styled.div`
+  display: inline-block;
+`
+
+const ColorItemBtnRemove = styled.button`
+  right: 0px;
+  border: none;
+  background-color: transparent;
+  ${TextColorStyle};
+`
+
+const BtnAction = styled.button`
+  border: none;
+  ${TextColorStyle};
+  background-color: transparent;
+  font-weight: 500;
+  text-transform: uppercase;
+  font-size: 0.8em;
+  margin: 5px 10px;
+  padding: 0px;
+`
+
+const Render = (props) => (
+  <ThemeProvider theme={props.theme}>
+    <BodyContainer style={{
+      backgroundColor: props.theme.color
+    }}>
+
+      <Header>
+        <Brand>Colorized</Brand>
+        <LeftActionsHeader>
+          <BtnAction onClick={props.randomColor}>random</BtnAction>
+        </LeftActionsHeader>
+      </Header>
+
+      {(props.paletteView) === false &&
+        <RenderViewSelectColor {...props} />
+      }
+
+      {props.paletteView === true &&
+        <RenderViewColors {...props} />
+      }
+
+      <Footer>
+        <a href='https://github.com/JonDotsoy/Colorized' target='_blank'>Github</a>
+        <a href='https://jon.soy/' target='_blank'>JON.SOY</a>
+      </Footer>
+
+    </BodyContainer>
+  </ThemeProvider>
+)
+
+const RenderViewSelectColor = ({theme, handleToggleViewPalette, handleAddColor}) => (
+  <ContainerTextColor>
+    <TextColor>
+      {theme.color}
+    </TextColor>
+    <MessagePressEspaceToRandomColor>Use space key to change color</MessagePressEspaceToRandomColor>
+  </ContainerTextColor>
+)
+
+const RenderViewColors = ({theme, handleToggleViewPalette, colors}) => (
+  <ContainerListColors>
+    { colors.map((color, index) => (
+      <ThemeProvider key={index} theme={{...theme, color}}>
+        <ColorItem key={index} style={{backgroundColor: color}}>
+          <ColorItemText>{color}</ColorItemText>
+          <ColorItemBtnRemove><MdClear /></ColorItemBtnRemove>
+        </ColorItem>
+      </ThemeProvider>
+    )) }
+  </ContainerListColors>
+)
+
+module.exports.Render = Render
